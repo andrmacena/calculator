99e1586632226ec6381aff61d8300e01c8657377
André Macena <andrmacena_@outlook.com>
2019-07-07 21:52:16 -0300

    Atualizado componente Button
    Foi atualizado a estilizaçao dos botoes para diferentes tipos: operação, numero

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,22 +2,37 @@ import React from 'react'
 import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native'
 
 const styles = StyleSheet.create({
-    button:{
+    button: {
         fontSize: 35,
         height: Dimensions.get('window').width / 4,
         width: Dimensions.get('window').width / 4,
         padding: 20,
-        backgroundColor: '#F0F0F0',
+        backgroundColor: '#FFF',
         textAlign: 'center',
         borderWidth: 1,
         borderColor: '#888'
+    },
+    operationButton: {
+        color: '#fa8231',
+        backgroundColor: '#F0F0F0'
+    },
+    buttonDouble: {
+        width: (Dimensions.get('window').width / 4) * 2
+    },
+    buttonTriple: {
+        width: (Dimensions.get('window').width / 4) * 3
     }
+
 })
 
 export default props => {
-    return (
-        <TouchableHighlight onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
-        </TouchableHighlight>
-    )
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton.push(styles.operationButton)
+        return (
+            <TouchableHighlight onPress={() => props.onClick(props.label)}>
+                <Text style={stylesButton}>{props.label}</Text>
+            </TouchableHighlight>
+        )
 }
